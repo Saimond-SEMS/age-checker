98ef23fbf95784b3a3acac236438c7327a84b50f
Saimond-SEMS <exactsameghost@gmail.com>
2025-02-13 18:24:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace itla3edad
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Bienenido");
+			Console.WriteLine("ingrese edad:");
+
+			int edad = int.Parse(Console.ReadLine());
+
+			if (edad < 0) {
+				Console.WriteLine("numero negativo");
+					
+			} else if (edad <= 12) {
+				Console.WriteLine("Etapa de infancia");
+					
+			} else if (edad <= 17) {
+				Console.WriteLine("Etapa de adolecencia");
+					
+			} else if (edad <= 65) {
+				Console.WriteLine("Etapa de adultez");
+					
+			} else if (edad > 65) {
+				Console.WriteLine("Etapa de vejez");
+			}
+		}
+	}
+}
